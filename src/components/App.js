8e510710a6fdd41a63d73fd79fd946f87c95d97d
Wongelemengist Yilma <wongel.yilma2016@gmail.com>
2022-08-22 11:13:57 +0300

    Setting up a default search term

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,17 @@ import VideoDetail from "./VideoDetail";
 import VideoList from "./VideoList";
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
+  componentDidMount() {
+    this.onSearchSubmit("buildings");
+  }
   onSearchSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: { q: term },
     });
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
     console.log(response);
   };
   onVideoSelect = (video) => {
